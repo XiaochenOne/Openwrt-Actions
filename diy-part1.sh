7c80cc437cddeb80a274da1a6ac14ce6ae2453b4
XiaochenOne <96930527+XiaochenOne@users.noreply.github.com>
2024-02-13 12:32:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone https://github.com/XiaochenOne/some-openwrt-s-package.git package/some-package
+#git clone https://github.com/XiaochenOne/some-openwrt-s-package.git package/some-package
