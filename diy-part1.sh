e407a116cdfd7aa2d5a9796eb4758ef5c48e89c1
XiaochenOne <96930527+XiaochenOne@users.noreply.github.com>
2024-08-07 11:10:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,6 @@
 
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-package
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall
+
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
